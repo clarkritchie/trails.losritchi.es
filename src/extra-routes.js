28ede82b9f745c4d2aba4220d7f6556ebe5826d7
Clark Ritchie <clarkritchie@gmail.com>
2024-08-04 11:58:05 -0700

    updates to trails

--- src/extra-routes.js
+++ src/extra-routes.js
@@ -1,7 +1,9 @@
 module.exports = [
-    "Tumalo Ridge > Sector 16 > Upper Whoops > Skyliner",
-	"Tumalo Ridge > Swede Ridge > Swampy > Southfork > Tumalo Creek",
-	"Storm King > Tiddlywinks > Larsen's > Tyler's Traverse > Catch & Release > Storm King",
-	"Tumalo Creek > Northfork > Metolius-Windigo > Mrazek > Farewell > Tumalo Creek",
-	"Alpine > Tire Mountain > Cloverpatch > Alpine (ATCA)"
+  "Tumalo Ridge > Sector 16 > Upper Whoops > Skyliner",
+  "Tumalo Ridge > Swede Ridge > Swampy > Southfork > Tumalo Creek",
+  "Storm King > Tiddlywinks > Larsen's > Tyler's Traverse > Catch & Release > Storm King",
+  "Tumalo Creek > Northfork > Metolius-Windigo > Mrazek > Farewell > Tumalo Creek",
+  "Alpine > Tire Mountain > Cloverpatch > Alpine (ATCA)",
+  "Pinedrops > Unknown > Phil's > Storm King > Grand Slam > VooDoo Child > VooDoo > Ben's",
+  "EXT > Phil's > VooDoo > VooDoo Child  > Grand Slam > Storm King > Unknown > Pinedrops",
 ];
